105ca80c641059985d04a72b41855a85a0588ab6
Sydana Schalk <sydana.schalk.2019@student.lhschools.org>
2018-10-05 14:18:39 -0400

    started designing

---
+++ src/MarioGame.java
@@ -0,0 +1,30 @@
+public class MarioGame {
+
+    public static void main(String[]args){
+
+        //Mario=rock
+            //Beats Bowser
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+        //luigi=paper
+        //Bowser=scissors
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
